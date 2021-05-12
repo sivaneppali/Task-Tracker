8df67de7cb550d274e6500db4cd2e1aa369ba1f4
sivaneppali <chinna.neppali@gmail.com>
2021-05-12 16:09:39 -0500

    Added Multiple Components and Routing and Mimicked Server through JSON-Server

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,15 +1,17 @@
 import propTypes from "prop-types"
+import {useLocation} from 'react-router-dom'
 import Button from "./Button"
 
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
+      {location.pathname==='/'&&<Button
         color={showAdd ? "red" : "green"}
         text={showAdd ? "close" : "Add"}
         onClick={onAdd}
-      />
+      />}
     </header>
   )
 }
